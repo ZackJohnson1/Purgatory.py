1feb4b08572b66d19dc306aa3bc1fdbbe0e64de5
zachjohnson <johnsza5@oregonstate.edu>
2023-01-20 18:47:22 -0800

    Push

---
+++ weight_divisions.py
@@ -0,0 +1,14 @@
+weight_divisions = [
+    ('WSW', 'W Strawweight'),
+    ('WFW', 'W Flyweight'),
+    ('WBW', 'W Bantamweight'),
+    ('WFW', 'W Featherweight'),
+    ('FW', 'Flyweight'),
+    ('BW', 'Bantamweight'),
+    ('FW', 'Featherweight'),
+    ('LW', 'Lightweight'),
+    ('WW', 'Welterweight'),
+    ('MW', 'Middleweightt'),
+    ('LHW', 'Light Heavyweight'),
+    ('HW', 'Heavyweight')
+    ]
